ba3a6729e25b8d4f518a0e1521878726d8c5ac13
Albertino Neto <albeneto72@gmail.com>
2023-10-21 20:51:02 -0300

    Inicio do projeto spring jdbc para jpa.

---
+++ src/main/java/br/com/estudo/jdbc/appspringjdbc/AppSpringJdbcJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudo.jdbc.appspringjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringJdbcJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringJdbcJpaApplication.class, args);
+	}
+
+}
